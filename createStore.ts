493a06fc033f92d906a98e03c0da7a61bfbae3ef
Hubert Bieszczad <48803618+Brentlok@users.noreply.github.com>
2023-12-15 16:25:40 +0100

    Add files via upload

---
+++ createStore.ts
@@ -0,0 +1,112 @@
+import equal from 'fast-deep-equal/es6'
+import { useMemo, useSyncExternalStore } from 'react'
+
+export type Synchronizer<T> = {
+    value: T,
+    subscribe: (update: (value: T) => void, key: string) => VoidFunction,
+    getSnapshot: (key: string) => T | null | undefined,
+    update: (value: T, key: string) => void
+}
+
+const capitalize = (str: string) => `${str.charAt(0).toUpperCase()}${str.slice(1)}`
+const isSynchronizer = (value: unknown): value is Synchronizer<unknown> => {
+    return typeof value === 'object' && value !== null && 'subscribe' in value && 'value' in value && 'update' in value && 'getSnapshot' in value
+}
+
+export const createStore = <TStateRaw extends object>(stateRaw: TStateRaw) => {
+    type TState = { [K in keyof TStateRaw]: TStateRaw[K] extends Synchronizer<infer U> ? U : TStateRaw[K] }
+
+    const storeKeys = Object.keys(stateRaw) as Array<keyof TState>
+
+    const actions = storeKeys.reduce((acc, key) => ({
+        ...acc,
+        [`set${capitalize(String(key))}`]: (value: TState[typeof key]) => {
+            state[key] = value
+            listeners[key].forEach(listener => listener(value))
+        }
+    }), {} as { [K in keyof TState as `set${Capitalize<K & string>}`]: (value: TState[K]) => void })
+
+    const listeners = storeKeys.reduce((acc, key) => ({
+        ...acc,
+        [key]: [],
+    }), {} as { [K in keyof TState]: Array<(newState: TState[K]) => void> })
+
+    const state = Object.entries(stateRaw).reduce((acc, [key, value]) => {
+        if (isSynchronizer(value)) {
+            value.subscribe(actions[`set${capitalize(key)}`], key)
+            listeners[key as keyof TState].push(newValue => value.update(newValue, key))
+            const snapshotValue = value.getSnapshot(key)
+
+            if (!snapshotValue && value.value !== undefined) {
+                value.update(value.value, key)
+            }
+
+            return {
+                ...acc,
+                [key]: snapshotValue ?? value.value
+            }
+        }
+
+        return {
+            ...acc,
+            [key]: value
+        }
+    }, {}) as { [K in keyof TState]: TState[K] }
+
+    const subscribe = <TKeys extends Array<keyof TState>>(keys: TKeys) => (listener: VoidFunction) => {
+        keys.forEach(key => listeners[key].push(listener))
+
+        return () => {
+            keys.forEach(key => {
+                listeners[key] = listeners[key].filter(l => l !== listener)
+            })
+        }
+    }
+
+    const getState = <TKeys extends Array<keyof TState>>(keys: TKeys) => {
+        let oldState: { [K in keyof TState]: TState[K] }
+
+        return () => {
+            const newState = keys.reduce((acc, key) => ({
+                ...acc,
+                [key]: state[key]
+            }), {} as { [K in keyof TState]: TState[K] })
+
+            if (equal(oldState, newState)) {
+                return oldState
+            }
+
+            oldState = newState
+
+            return newState
+        }
+    }
+
+    const getActions = <TKeys extends Array<keyof TState>>(keys: TKeys) => {        
+        return keys.reduce((acc, key) => {
+            const actionKey = `set${capitalize(String(key))}`
+
+            return {
+                ...acc,
+                [actionKey]: actions[actionKey as keyof typeof actions] 
+            }
+        }, {} as { [K in keyof TState as `set${Capitalize<K & string>}`]: (value: TState[K]) => void })
+    }
+
+    const useStore = <TKeys extends Array<keyof TState>>(...keys: [...TKeys]) => {
+        const getSnapshot = useMemo(() => getState(keys), [])
+        const actions = useMemo(() => getActions(keys), [])
+        const state = useSyncExternalStore(subscribe(keys), getSnapshot)
+
+        return {
+            state,
+            actions
+        }
+    }
+
+    return {
+        useStore,
+        getState: () => state,
+        actions: getActions(storeKeys),
+    }
+}
---
+++ storage.ts
@@ -0,0 +1,40 @@
+import { Synchronizer } from './createStore'
+
+export function storage <T>(initialValue?: T, localStorageKey?: string): Synchronizer<T | undefined>
+export function storage <T>(initialValue: T, localStorageKey?: string) {
+    return {
+        value: initialValue,
+        subscribe: (update, key) => {
+            const handleEvent = (event: StorageEvent) => {
+                const storageKey = localStorageKey ?? key
+
+                if (event.key !== storageKey || event.newValue === null) {
+                    return
+                }
+
+                update(JSON.parse(event.newValue))
+            }
+
+            window.addEventListener('storage', handleEvent)
+
+            return () => {
+                window.removeEventListener('storage', handleEvent)
+            }
+        },
+        update: (value: T, key) => {
+            const storageKey = localStorageKey ?? key
+
+            localStorage.setItem(storageKey, JSON.stringify(value))
+        },
+        getSnapshot: key => {
+            const storageKey = localStorageKey ?? key
+            const value = localStorage.getItem(storageKey)
+
+            if (value === null) {
+                return null
+            }
+
+            return JSON.parse(value)
+        }
+    } as Synchronizer<T>
+}
