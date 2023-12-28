068c7d1b9db600c83f5a13aa09db44e20be86c20
Grzegorz <tarnopolskigrzegorz@gmail.com>
2023-12-28 15:19:57 +0100

    fix: test

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,11 @@ module.exports = {
     transform: {
         '^.+\\.(js|ts)$': 'ts-jest',
     },
+    transformIgnorePatterns: [
+        '/node_modules/(?![@autofiy/autofiyable|@autofiy/property]).+\\.js$',
+        '/node_modules/(?![@autofiy/autofiyable|@autofiy/property]).+\\.ts$',
+        '/node_modules/(?![@autofiy/autofiyable|@autofiy/property]).+\\.tsx$',
+    ],
     coverageReporters: [
         'json-summary',
         'text',
