7abf5bf3d6eda9b7a53720b2ab085900b8ee5e68
Hubert Bieszczad <brentlokk@gmail.com>
2024-04-24 08:58:15 +0200

    feat: change build method to unbuild

--- build.ts
+++
@@ -1,9 +0,0 @@
-import Bun, { $ } from 'bun'
-
-await $`rm -rf dist`
-await Bun.build({
-    entrypoints: ['./src/index.ts', './src/mmkv/index.ts', './src/storage/index.ts'],
-    outdir: 'dist',
-    external: ['react-native-mmkv', 'react'],
-    minify: true,
-})
