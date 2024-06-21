f8e6331c48166ce38c26dd75dd4bc30a69ab2c67
Hubert Bieszczad <48803618+Brentlok@users.noreply.github.com>
2024-06-21 15:26:26 +0200

    Starlight docs (#69)
    Create stan-js docs using starlight

---
+++ docs/README.md
@@ -0,0 +1,6 @@
+# stan-js docs
+
+```sh
+bun install
+bun run dev
+```
---
+++ docs/astro.config.mjs
@@ -0,0 +1,37 @@
+import starlight from '@astrojs/starlight'
+import { defineConfig } from 'astro/config'
+
+export default defineConfig({
+    site: 'https://codemask-labs.github.io',
+    base: 'stan-js',
+    integrations: [
+        starlight({
+            title: 'stan-js',
+            logo: {
+                src: './src/assets/logo.svg',
+            },
+            social: {
+                github: 'https://github.com/codemask-labs/stan-js',
+            },
+            sidebar: [
+                {
+                    label: 'Guides',
+                    autogenerate: { directory: 'guides' },
+                },
+                {
+                    label: 'Reference',
+                    autogenerate: { directory: 'reference' },
+                },
+                {
+                    label: 'Examples',
+                    autogenerate: { directory: 'examples' },
+                },
+                {
+                    label: 'Codemask',
+                    link: 'https://codemask.com',
+                },
+            ],
+            customCss: ['./src/styles.css'],
+        }),
+    ],
+})
