5a5a2a657c630917242f43692a32ee7547e6a08c
Hubert Bieszczad <brentlokk@gmail.com>
2024-06-21 15:25:47 +0200

    chore: update for deployment

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,6 +2,8 @@ import starlight from '@astrojs/starlight'
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
+    site: 'https://codemask-labs.github.io',
+    base: 'stan-js',
     integrations: [
         starlight({
             title: 'stan-js',
