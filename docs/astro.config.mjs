4aac3b803007f4aca315ca312d06323228229bf9
Hubert Bieszczad <brentlokk@gmail.com>
2024-06-21 15:10:05 +0200

    feat: stan-js docs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,27 +1,35 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import starlight from '@astrojs/starlight'
+import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
-});
+    integrations: [
+        starlight({
+            title: 'stan-js',
+            logo: {
+                src: './src/assets/logo.svg',
+            },
+            social: {
+                github: 'https://github.com/codemask-labs/stan-js',
+            },
+            sidebar: [
+                {
+                    label: 'Guides',
+                    autogenerate: { directory: 'guides' },
+                },
+                {
+                    label: 'Reference',
+                    autogenerate: { directory: 'reference' },
+                },
+                {
+                    label: 'Examples',
+                    autogenerate: { directory: 'examples' },
+                },
+                {
+                    label: 'Codemask',
+                    link: 'https://codemask.com',
+                },
+            ],
+            customCss: ['./src/styles.css'],
+        }),
+    ],
+})
