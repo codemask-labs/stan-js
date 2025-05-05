d79b3d4619344c02526e29fcfb1088f3bf05d078
Hubert Bieszczad <48803618+Brentlok@users.noreply.github.com>
2025-05-05 08:22:57 +0200

    chore: update all configs to ts files (#95)

--- examples/react/vite.config.ts
+++ examples/react/vite.config.ts
@@ -1,9 +1,13 @@
+import tailwind from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        tailwind(),
+    ],
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
